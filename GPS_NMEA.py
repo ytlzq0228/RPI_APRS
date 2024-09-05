ba00d0b02d455d66ab219bc4115cbeac747093c4
Doubi Lee <42897239+ytlzq0228@users.noreply.github.com>
2024-09-05 10:53:20 +0800

    Update GPS_NMEA.py

--- GPS_NMEA.py
+++ GPS_NMEA.py
@@ -13,6 +13,7 @@ ser=serial.Serial(com_port, baud_rate, bytesize=serial.EIGHTBITS, parity=serial.
 
 def save_log(result):
 	try:
+		print(result)
 		now = datetime.now().strftime('%d-%m-%Y %H:%M:%S')
 		f = open("/var/log/GPS_NMEA.log",'a')
 		f.writelines("\n%s:log:%s" %(now,result))
@@ -41,7 +42,6 @@ def NMEA_GPGGA(sentence):
 			save_log(sentence)
 			return lat_dd,lat_dir,lon_dd,lon_dir,altitude,timestamp
 		else:
-			print("No %s Signal. Waiting....."%parts[0])
 			save_log("No %s Signal. Waiting....."%parts[0])
 	return None,None,None,None,None,None
 
@@ -56,7 +56,6 @@ def NMEA_GPRMC(sentence):
 			save_log(sentence)
 			return speed,course
 		else:
-			print("No %s Signal. Waiting....."%parts[0])
 			save_log("No %s Signal. Waiting....."%parts[0])
 	return None,None
 
@@ -69,7 +68,7 @@ def get_gnss_position():
 				line='$GPGGA,024438.00,4004.6300,N,11618.2178,E,01,07,10.3,20.05,M,-15.40,M,1.1,1023*63<CR><LF>'
 				lat,lat_dir,lon,lon_dir,altitude,timestamp=NMEA_GPGGA(line)
 				if lat is not None and lon is not None and altitude is not None:
-					print(f"GNSS GPGGA: lat={lat}, lon={lon}, altitude/feet={altitude}")
+					save_log(f"GNSS GPGGA: lat={lat}, lon={lon}, altitude/feet={altitude}")
 					break
 			#time.sleep(0.1)
 		i=0
@@ -81,7 +80,7 @@ def get_gnss_position():
 				line='$GPRMC,123519,A,4807.038,N,01131.000,E,010.4,084.4,230394,003.1,W*6A'
 				speed,course=NMEA_GPRMC(line)
 				if speed is not None and course is not None:
-					print(f"GNSS GPRMC: speed/knots={speed}, course={course}")
+					save_log(f"GNSS GPRMC: speed/knots={speed}, course={course}")
 					break
 			#time.sleep(0.1)
 			i+=1
@@ -96,7 +95,7 @@ if __name__ == '__main__':
 			lat,lat_dir,lon,lon_dir,altitude,timestamp,speed,course=get_gnss_position()
 			frame_text=('BI1FQO-P>APDG03,TCPIP*,qAC,BI1FQO-RS:!%s%s/%s%s>%s/%s/A=%s Auto Report by RPI with GPS module at UTC %s on 逗老师的Xiaomi Su7 Max'%(lat,lat_dir,lon,lon_dir,course,speed,altitude,timestamp)).encode()
 			a=aprs.TCP(b'BI1FQO', b'20898')
-			print(a.start())
+			a.start()
 			save_log(a.send(frame_text))
 			time.sleep(15)
 		except Exception as err:
