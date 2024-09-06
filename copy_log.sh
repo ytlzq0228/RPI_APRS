e01fd27354caa3f06068fcbfb621aa1f802364fd
liziqi <123>
2024-09-06 16:34:27 +0800

    i

--- copy_log.sh
+++ copy_log.sh
@@ -2,22 +2,24 @@
 
 # 设置日志文件路径
 LOG_FILE="/var/log/GPS_NMEA.log"
+
+# 获取当前日期时间作为文件前缀（格式：yyyy-mm-dd-hh-mm-ss）
+DATE_PREFIX=$(date +"%Y-%m-%d-%H-%M-%S")
 # 生成目标文件名
 REMOTE_FILE="${DATE_PREFIX}_GPS_NMEA.log"
 
 
 # 设置远程服务器信息
 REMOTE_USER="root"
-REMOTE_HOST="10.0.6.10"
+REMOTE_HOST="nas.ctsdn.com"
 REMOTE_DIR="/volume1/Storage/Su7-GPS-PATH"
 
-# 获取当前日期时间作为文件前缀（格式：yyyy-mm-dd-hh-mm-ss）
-DATE_PREFIX=$(date +"%Y-%m-%d-%H-%M-%S")
+
 
 
 
 # 拷贝文件到远程服务器
-scp $LOG_FILE ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}/${REMOTE_FILE}
+scp -P 10223 $LOG_FILE ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}/${REMOTE_FILE}
 
 # 检查拷贝是否成功
 if [ $? -eq 0 ]; then
