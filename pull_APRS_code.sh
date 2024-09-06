f87054d1e5a6c7b1a739c4d31feec475b4f38a82
liziqi <123>
2024-09-06 16:07:48 +0800

    i

--- pull_APRS_code.sh
+++ pull_APRS_code.sh
@@ -16,16 +16,12 @@ done
 
 sudo mount -o remount,rw / ; sudo mount -o remount,rw /boot
 
-# 定义你的项目路径
 PROJECT_DIR="/home/pi-star/RPI_APRS"
 
-# 进入项目目录
 cd "$PROJECT_DIR" || exit
 
-# 拉取最新代码
 git pull origin main
 
-# 记录拉取操作的时间和结果
 echo "Code pulled on $(date)" >> /var/log/git_pull.log
 
 sudo sync ; sudo sync ; sudo sync ; sudo mount -o remount,ro / ; sudo mount -o remount,ro /boot
