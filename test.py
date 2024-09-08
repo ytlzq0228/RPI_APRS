b4622fda21de078959a1d0f4d06caeea5e0b0331
liziqi <123>
2024-09-08 19:22:31 +0800

    i

--- test.py
+++ test.py
@@ -102,7 +102,7 @@ if __name__ == '__main__':
 					save_log(f"Retrying get_gnss_position due to error: {err}")
 					time.sleep(1)  # 等待1秒后重试
 			frame_text=('BI1FQO-MI>PYTHON,TCPIP*,qAC,BI1FQO-MI:!%s%s/%s%s>%s/%s/A=%s APRS by RPI with GPS at UTC %s on 逗老师的Xiaomi Su7 Max,see more https://ctsdn.blog.csdn.net/article/details/130228867'%(lat,lat_dir,lon,lon_dir,course,speed,altitude,timestamp)).encode()
-			a=aprs.TCP(b'BI1FQO', b'20898',b'asia.aprs2.net')
+			a=aprs.TCP(b'BI1FQO', b'20898',b'asia.aprs2.net',14580)
 			a.start()
 			save_log(a.send(frame_text))
 			time.sleep(30)
