8bc3c389523f8bc438fdf3840023c4c193a36957
liziqi <123>
2024-09-08 14:14:32 +0800

    i

--- GPS_NMEA.py
+++ GPS_NMEA.py
@@ -66,7 +66,7 @@ def get_gnss_position():
 			if ser.in_waiting > 0:
 				line=ser.readline().decode('ascii', errors='replace').strip()  # 读取一行NMEA数据
 				#line='$GPGGA,041824.00,4004.6300,N,11618.2178,E,01,07,10.3,20.05,M,-15.40,M,1.1,1023*63<CR><LF>' #for testing
-				#save_log(f"GPGGA Line:{line}")
+				save_log(f"GPGGA Line:{line}")
 				lat,lat_dir,lon,lon_dir,altitude,timestamp=NMEA_GPGGA(line)
 				if lat is not None and lon is not None and altitude is not None:
 					save_log(f"GNSS GPGGA: lat={lat}, lon={lon}, altitude/feet={altitude}")
