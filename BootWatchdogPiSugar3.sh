9abba6b47b8e17571b32598dbcbde69739754f91
liziqi <123>
2024-09-26 22:18:00 +0800

    0926.01

--- BootWatchdogPiSugar3.sh
+++ BootWatchdogPiSugar3.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
-source /etc/GPS_config.cfg
-# 判断 Test_Flag 是否等于 0
-if [ "$Test_Flag" -eq 0 ]; then
-    # 如果 Test_Flag 是 0，则执行 sleep 30
-    sleep 60
-fi
+#source /etc/GPS_config.cfg
+## 判断 Test_Flag 是否等于 0
+#if [ "$Test_Flag" -eq 0 ]; then
+#    # 如果 Test_Flag 是 0，则执行 sleep 30
+#    sleep 60
+#fi
 
 
 
 
 TMP=$(i2cget -y 1 0x57 0x06)
-#echo $TMP
-#开机看门狗开启并喂狗 Turn on the watchdog and feed the dog
+echo $TMP
+echo "TMP $TMP" >> /var/log/git_pull.log
+echo "开机看门狗开启并喂狗 Turn on the watchdog and feed the dog" >> /var/log/git_pull.log
 RST=$((0x98 | TMP ))
-#echo $RST
+echo "RST $RST" >> /var/log/git_pull.log
 #设置最大重启次数 Set the maximum number of restarts
 i2cset -y 1 0x57 0x0a 10 
+TMP=$(i2cget -y 1 0x57 0x06)
+echo $TMP
+echo "TMP $TMP" >> /var/log/git_pull.log
 #写入寄存器 Write register
 i2cset -y 1 0x57 0x06 $RST 
+TMP=$(i2cget -y 1 0x57 0x06)
+echo $TMP
+echo "TMP $TMP" >> /var/log/git_pull.log
 
 #该脚本应当设置为开机启动。每次开机只需要运行一次 The script should be set to boot. You only need to run it once per boot
