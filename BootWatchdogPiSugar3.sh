c3f257fd2f390b23a7e3ab5792ecdb4c87339de9
liziqi <123>
2024-09-26 19:27:08 +0800

    0926.01

--- BootWatchdogPiSugar3.sh
+++ BootWatchdogPiSugar3.sh
@@ -13,7 +13,7 @@ fi
 TMP=$(i2cget -y 1 0x57 0x06)
 #echo $TMP
 #开机看门狗开启并喂狗 Turn on the watchdog and feed the dog
-RST=$((0x18 | TMP ))
+RST=$((0x98 | TMP ))
 #echo $RST
 #设置最大重启次数 Set the maximum number of restarts
 i2cset -y 1 0x57 0x0a 10 
@@ -21,14 +21,3 @@ i2cset -y 1 0x57 0x0a 10
 i2cset -y 1 0x57 0x06 $RST 
 
 #该脚本应当设置为开机启动。每次开机只需要运行一次 The script should be set to boot. You only need to run it once per boot
-
-TMP=$(i2cget -y 1 0x57 0x06)
-#echo $TMP
-#看门狗开启WatchdogOn
-RST=$((0x80 | TMP ))
-#echo $RST
-#设置超时时长10*2s Set timeout duration 10 * 2S
-i2cset -y 1 0x57 0x07 10
-#写入寄存器 Write register
-i2cset -y 1 0x57 0x06 $RST
-#i2cdump -y 1 0x57
