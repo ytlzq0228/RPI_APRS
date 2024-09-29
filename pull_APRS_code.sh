afd75ce64fc6a97cb7b551928c28a5f7f2ef2293
liziqi <123>
2024-09-29 19:49:57 +0800

    0929.02

--- pull_APRS_code.sh
+++ pull_APRS_code.sh
@@ -5,23 +5,22 @@ source /etc/GPS_config.cfg
 
 i2cset -y 1 0x57 0x06 0x18
 
+sudo mount -o remount,rw / ; sudo mount -o remount,rw /boot
+
+PROJECT_DIR="/home/pi-star/RPI_APRS"
+
+cd "$PROJECT_DIR" || exit
+
 if [ "$OLED_Enable" -eq 1 ]; then
     echo "booting $(date)" >> /var/log/git_pull.log
-    python3 /home/pi-star/RPI_APRS/SSD1306_booting.py
+    python3 SSD1306_booting.py
 fi
-
 
 # 判断 Test_Flag 是否等于 0
 if [ "$Test_Flag" -eq 0 ]; then
     # 如果 Test_Flag 是 0，则执行 sleep 30
     sleep 30
 fi
-
-sudo mount -o remount,rw / ; sudo mount -o remount,rw /boot
-
-PROJECT_DIR="/home/pi-star/RPI_APRS"
-
-cd "$PROJECT_DIR" || exit
 
 git reset --hard
 git pull origin main
