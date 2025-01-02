3890aaa9d6a411175a192fc67b8e624b8a3c8896
liziqi <123>
2025-01-02 15:39:39 +0800

    1

--- pull_APRS_code.sh
+++ pull_APRS_code.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-source /etc/GPS_config.cfg
-#alias rpi-ro='sudo sync ; sudo sync ; sudo sync ; sudo mount -o remount,ro / ; sudo mount -o remount,ro /boot'
-#alias rpi-rw='sudo mount -o remount,rw / ; sudo mount -o remount,rw /boot'
+
+CONFIG_FILE='/etc/GPS_config.ini'
+# 读取配置函数
+function get_config() {
+    local section=$1
+    local key=$2
+    grep -A 10 "^\[$section\]" "$CONFIG_FILE" | grep "^$key" | awk -F '=' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//'
+}
 
 i2cset -y 1 0x57 0x06 0x18
 
@@ -11,13 +16,13 @@ PROJECT_DIR="/home/pi-star/RPI_APRS"
 
 cd "$PROJECT_DIR" || exit
 
-if [ "$OLED_Enable" -eq 1 ]; then
+if [ $(get_config "OLED_Config" "OLED_Enable") -eq True ]; then
     echo "booting $(date)" >> /var/log/git_pull.log
     python3 SSD1306_booting.py
 fi
 
 # 判断 Test_Flag 是否等于 0
-if [ "$Test_Flag" -eq 0 ]; then
+if [ $(get_config "Test_Flag" "enable") -eq True ]; then
     # 如果 Test_Flag 是 0，则执行 sleep 30
     sleep 5
 fi
@@ -29,4 +34,4 @@ echo "Code pulled on $(date)" >> /var/log/git_pull.log
 
 sudo sync ; sudo sync ; sudo sync ; sudo mount -o remount,ro / ; sudo mount -o remount,ro /boot
 echo "$SSID"
-python3 GPS_NMEA.py "$Test_Flag" "$SSID" "$Message" "$SSID_ICON" "$OLED_Enable" "$OLED_Address" "$GPS_Device" "$GPS_Option"
+python3 GPS_NMEA.py 
