a5f20a9e3d128bfae4bfbd4ee1986d3cae68135d
liziqi <123>
2025-01-02 19:07:25 +0800

    1

--- GNSS_NMAE.py
+++ GNSS_NMAE.py
@@ -195,8 +195,8 @@ class Get_GNSS_Position:
 			for new_data in gps_socket:
 				if new_data:
 					print(type(new_data))
-					data_stream.unpack(new_data)
-					print(data_stream)
+					#data_stream.unpack(new_data)
+					#print(data_stream)
 					#lat = "%.2f"%float(data_stream.TPV['lat'])
 					#lon = "%.2f"%float(data_stream.TPV['lon'])
 					#altitude = altitude="%06.0f"%float(data_stream.TPV['alt'])
