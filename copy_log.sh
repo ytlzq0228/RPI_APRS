58d773bfbb4340237fac34aa5c9b2f12a57dc905
liziqi <123>
2025-01-02 22:51:32 +0800

    1

--- copy_log.sh
+++ copy_log.sh
@@ -111,12 +111,12 @@ LOG_FILE="/var/log/GPS_NMEA.log"
 # 获取当前日期时间作为文件前缀（格式：yyyy-mm-dd-hh-mm-ss）
 DATE_PREFIX=$(date +"%Y-%m-%d-%H-%M-%S")
 # 生成目标文件名
-REMOTE_FILE="${DATE_PREFIX}_GPS_$(get_config "SSID_Config" "SSID").log"
+REMOTE_FILE="${DATE_PREFIX}_GPS_${(get_config "SSID_Config" "SSID")}.log"
 
 
 
 
-[SFTP]
+
 # 设置远程服务器信息
 REMOTE_USER=$(get_config "SFTP_Config" "REMOTE_USER")
 REMOTE_HOST=$(get_config "SFTP_Config" "REMOTE_HOST")
