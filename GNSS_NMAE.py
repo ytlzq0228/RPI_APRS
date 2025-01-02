b370b0ee120dc3ced06616daac945531e6e75cd5
liziqi <123>
2025-01-02 19:01:52 +0800

    1

--- GNSS_NMAE.py
+++ GNSS_NMAE.py
@@ -183,7 +183,7 @@ class Get_GNSS_Position:
 			raise
 
 
-	def GPSd(Test_Flag,tcp_host,tcp_port):
+	def GPSd2(Test_Flag,tcp_host,tcp_port):
 		"""通过 gps3 获取 GPS 数据"""
 		gps_socket = gps3.GPSDSocket()
 		data_stream = gps3.DataStream()
@@ -210,7 +210,42 @@ class Get_GNSS_Position:
 			print("Exiting...")
 		except Exception as e:
 			print(f"Error fetching GPS data: {e}")
+
+	def GPSd(Test_Flag,tcp_host,tcp_port):
+	    """通过 gps3 获取 GPS 数据"""
+	    gps_socket = gps3.GPSDSocket()
+	    data_stream = gps3.DataStream()
+	    
+	    # 连接到 GPSd
+	    gps_socket.connect(host="127.0.0.1", port=2947)
+	    gps_socket.watch()
 	
+	    try:
+	        for new_data in gps_socket:
+	            if new_data:
+	                print(new_data)
+	                data_stream.unpack(new_data)
+	                print(data_stream)
+	                latitude = data_stream.TPV['lat']
+	                longitude = data_stream.TPV['lon']
+	                altitude = data_stream.TPV['alt']
+	                speed = data_stream.TPV['speed']
+	                timestamp = data_stream.TPV['time']
+	
+	                if latitude and longitude:
+	                    print(f"Time: {timestamp}")
+	                    print(f"Latitude: {latitude}°")
+	                    print(f"Longitude: {longitude}°")
+	                    print(f"Altitude: {altitude} m")
+	                    print(f"Speed: {speed} m/s")
+	                else:
+	                    print("Waiting for GPS signal...")
+	    except KeyboardInterrupt:
+	        print("Exiting...")
+	    except Exception as e:
+	        print(f"Error fetching GPS data: {e}")
+
+
 #{
 #    "class": "TPV",
 #    "device": "tcp://10.0.6.116:12321",
