5fbfa4cb132d952d83a8a52f184979a5eb164ca8
liziqi <123>
2025-01-02 19:08:50 +0800

    1

--- GNSS_NMAE.py
+++ GNSS_NMAE.py
@@ -3,6 +3,7 @@ import os
 import time
 import re
 import serial
+import json
 from gps3 import gps3
 from datetime import datetime
 import socket
@@ -194,7 +195,7 @@ class Get_GNSS_Position:
 		try:
 			for new_data in gps_socket:
 				if new_data:
-					print(type(new_data))
+					print(json.load(new_data))
 					#data_stream.unpack(new_data)
 					#print(data_stream)
 					#lat = "%.2f"%float(data_stream.TPV['lat'])
@@ -225,7 +226,6 @@ class Get_GNSS_Position:
 	            if new_data:
 	                print(new_data)
 	                data_stream.unpack(new_data)
-	                print(data_stream)
 	                latitude = data_stream.TPV['lat']
 	                longitude = data_stream.TPV['lon']
 	                altitude = data_stream.TPV['alt']
