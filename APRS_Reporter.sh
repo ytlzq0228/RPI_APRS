bbf37f9da949b0a4e15a85c593498af73f0250a6
liziqi <123>
2025-01-03 20:29:44 +0800

    1

---
+++ APRS_Reporter.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+CONFIG_FILE='/etc/GPS_config.ini'
+# 读取配置函数
+function get_config() {
+    local section=$1
+    local key=$2
+    grep -A 10 "^\[$section\]" "$CONFIG_FILE" | grep "^$key" | awk -F '=' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//'
+}
+
+i2cset -y 1 0x57 0x06 0x18
+
+PROJECT_DIR="/home/pi-star/RPI_APRS"
+
+cd "$PROJECT_DIR" || exit
+
+if [ $(get_config "OLED_Config" "OLED_Enable") = "True" ]; then
+    echo "booting $(date)" >> /var/log/git_pull.log
+    python3 SSD1306_booting.py
+fi
+
+# 判断 Test_Flag 是否等于 0
+if [ $(get_config "Test_Flag" "enable") != "True" ]; then
+    # 如果 Test_Flag 是 0，则执行 sleep 30
+    sleep 30
+fi
+
+echo $(get_config "SSID_Config" "SSID")
+echo "start python $(date)" >> /var/log/git_pull.log
+python3 APRS_Reporter.py 
+
--- pull_APRS_code.sh
+++ pull_APRS_code.sh
@@ -8,32 +8,15 @@ function get_config() {
     grep -A 10 "^\[$section\]" "$CONFIG_FILE" | grep "^$key" | awk -F '=' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//'
 }
 
+pkill -f "python3 APRS_Reporter.py"
+
 i2cset -y 1 0x57 0x06 0x18
 
 sudo mount -o remount,rw / ; sudo mount -o remount,rw /boot
 
-PROJECT_DIR="/home/pi-star/RPI_APRS"
-
-cd "$PROJECT_DIR" || exit
-
-if [ $(get_config "OLED_Config" "OLED_Enable") = "True" ]; then
-    echo "booting $(date)" >> /var/log/git_pull.log
-    python3 SSD1306_booting.py
-fi
-
-# 判断 Test_Flag 是否等于 0
-if [ $(get_config "Test_Flag" "enable") != "True" ]; then
-    # 如果 Test_Flag 是 0，则执行 sleep 30
-    sleep 30
-fi
-
 git reset --hard
 git pull origin main
 
-echo "Code pulled on $(date)" >> /var/log/git_pull.log
 
 sudo sync ; sudo sync ; sudo sync ; sudo mount -o remount,ro / ; sudo mount -o remount,ro /boot
-echo $(get_config "SSID_Config" "SSID")
-echo "start python $(date)" >> /var/log/git_pull.log
-python3 APRS_Reporter.py 
 
