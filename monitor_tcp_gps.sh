46de8da86ee901cde6bfd72e565df4bf4e109827
liziqi <123>
2025-01-21 21:56:35 +0800

    0121.02

--- monitor_tcp_gps.sh
+++ monitor_tcp_gps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-
 CONFIG_FILE='/etc/GPS_config.ini'
+
 # 读取配置函数
 function get_config() {
     local section=$1
@@ -9,7 +9,8 @@ function get_config() {
     grep -A 10 "^\[$section\]" "$CONFIG_FILE" | grep "^$key" | awk -F '=' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//'
 }
 
-# 配置 TCP 源地址和监控间隔
+# 获取配置文件中的关键值
+GPS_DEVICE=$(get_config "GPS_Config" "GPS_Device")
 TCP_SOURCE=$(get_config "GPS_Config" "GPSd_TCP_SOURCE")
 CHECK_INTERVAL=10  # 每 10 秒检查一次
 
@@ -24,8 +25,14 @@ check_tcp_source() {
     fi
 }
 
-# 主循环
-while true; do
-    check_tcp_source
-    sleep "$CHECK_INTERVAL"
-done
+# 检查是否需要监控
+if [[ "$GPS_DEVICE" == "GPSd" && -n "$TCP_SOURCE" ]]; then
+    echo "$(date): 配置文件指示使用 GPSd 并配置了 TCP 源 $TCP_SOURCE。开始监控..."
+    while true; do
+        check_tcp_source
+        sleep "$CHECK_INTERVAL"
+    done
+else
+    echo "$(date): 配置文件未指示使用 GPSd 或未配置 TCP 源。无需监控。"
+    exit 0
+fi
