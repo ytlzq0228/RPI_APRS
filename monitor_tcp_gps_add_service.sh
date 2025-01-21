62638d581d7c4fdc9ae2a3c5fcd8b2546237c293
liziqi <123>
2025-01-21 22:05:27 +0800

    0121.02

--- monitor_tcp_gps_add_service.sh
+++ monitor_tcp_gps_add_service.sh
@@ -5,4 +5,5 @@ sudo cp /etc/RPI_APRS/monitor_tcp_gps.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable monitor_tcp_gps.service
 sudo systemctl start monitor_tcp_gps.service
+sudo systemctl status monitor_tcp_gps.service
 sync ; sudo sync ; sudo sync ; sudo mount -o remount,ro / ; sudo mount -o remount,ro /boot
