22e2f89cab73368b7ba3ed8b7ae81b6e97bf5fb0
liziqi <123>
2025-02-19 22:06:55 +0800

    0219.01

--- RTK_Service.py
+++ RTK_Service.py
@@ -82,6 +82,7 @@ def main():
                     print(gga_message)
                     if send_gga_to_ntrip(gga_message, ntrip_socket):
                         response = ntrip_socket.recv(4096)  # 接收RTK修正数据
+                        print(response)
                         # 将RTK数据通过I2C写入到GNSS模块
                         write_data_to_gnss(response)
         except KeyError:
