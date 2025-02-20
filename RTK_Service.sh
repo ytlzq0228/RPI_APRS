b3ac5b44587d5b4d3f910ef96cde324bd6604881
liziqi <123>
2025-02-21 01:04:41 +0800

    0221.01

--- RTK_Service.sh
+++ RTK_Service.sh
@@ -1,1 +1,11 @@
-python3 /etc/RPI_APRS/RTK_Service.py -u qxymtq002 -p c4bcfd9 rtk.ntrip.qxwz.com 8002 AUTO
+#!/bin/bash
+
+CONFIG_FILE='/etc/GPS_config.ini'
+# 读取配置函数
+function get_config() {
+    local section=$1
+    local key=$2
+    grep -A 10 "^\[$section\]" "$CONFIG_FILE" | grep "^$key" | awk -F '=' '{print $2}' | sed 's/^[ \t]*//;s/[ \t]*$//'
+}
+
+python3 /etc/RPI_APRS/RTK_Service.py -u $(get_config "RTK_CONFIG" "USERNAME") -p $(get_config "RTK_CONFIG" "PASSWORD") $(get_config "RTK_CONFIG" "NTRIP_SERVER") $(get_config "RTK_CONFIG" "NTRIP_PORT") $(get_config "RTK_CONFIG" "MOUNTPOINT")
