22acc23f38e46a8153733d8ea7a9a8cb90035d60
liziqi <123>
2025-02-24 07:58:07 +0800

    0223.02

--- APRS_Reporter.py
+++ APRS_Reporter.py
@@ -148,7 +148,7 @@ if __name__ == '__main__':
 
 			if float(timestamp)-float(update_timestamp)>=NMEA_LOG_INTERVAL:
 				update_timestamp=timestamp
-				save_log(f"gpx:{lat_raw,lat_dir,lon_raw,lon_dir,altitude,timestamp,speed,course,GPS_Source,get_cpu_temperature(),get_uptime()}")
+				save_log(f"gpx:{lat_raw,lat_dir,lon_raw,lon_dir,altitude,timestamp,speed,course,GPS_Source,GNSS_Type,get_cpu_temperature(),get_uptime()}")
 
 			if float(timestamp)-float(report_timestamp)>=APRS_REPORT_INTERVAL and read_gpio(Radio_CONTROL_ENABLE,GPIO_PIN):
 				report_timestamp=timestamp
