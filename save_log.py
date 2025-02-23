30ea4ea0709d38e3ef063c458cf19d7cf2403fc9
liziqi <123>
2025-02-23 11:28:40 +0800

    0223.02

--- save_log.py
+++ save_log.py
@@ -11,7 +11,7 @@ config.read(CONFIG_FILE)
 
 LOG_FILE_PATH=config['SFTP_Config']['LOCAL_LOG_FILE_PATH']
 SSID=config['SSID_Config']['SSID']
-LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d')}-{LOG_FILE_PATH}-{SSID}"
+LOG_FILE = f"{LOG_FILE_PATH}/{datetime.now().strftime('%Y-%m-%d')}-GPS-{SSID}"
 
 
 def save_log(result):
