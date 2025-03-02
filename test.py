32dcaedeb4eb9b811e59a15668e342b19aae61a2
liziqi <123>
2025-03-02 16:03:12 +0800

    0302.01

--- test.py
+++ test.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify
-import gpsd
+from gps3 import gps3
 
-# 连接到本地GPSd
-gpsd.connect()
+# 创建GPSD连接
+gps_socket = gps3.GPSDSocket()
+data_stream = gps3.DataStream()
+gps_socket.connect()
+gps_socket.watch()
 
 app = Flask(__name__)
 
@@ -25,14 +28,16 @@ def index():
 @app.route('/gps-data')
 def gps_data():
     try:
-        packet = gpsd.get_current()
-        data = {
-            'latitude': packet.lat,
-            'longitude': packet.lon,
-            'altitude': packet.alt,
-            'speed': packet.hspeed,
-            'satellites': packet.sats
-        }
+        new_data = gps_socket.next()  # 获取新的GPS数据
+        if new_data:
+            data_stream.unpack(new_data)
+            data = {
+                'latitude': data_stream.TPV['lat'],
+                'longitude': data_stream.TPV['lon'],
+                'altitude': data_stream.TPV['alt'],
+                'speed': data_stream.TPV['speed'],
+                'satellites': len(data_stream.satellites)
+            }
     except Exception as e:
         data = {'error': str(e)}
     return jsonify(data)
