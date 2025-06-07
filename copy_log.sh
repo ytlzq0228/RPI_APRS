9c03db8956c9564d6d763be55ffef0d0524b1bfc
liziqi <123>
2025-06-07 15:37:51 +0800

    0607.01

--- copy_log.sh
+++ copy_log.sh
@@ -30,7 +30,7 @@ REMOTE_HOST=$(get_config "SFTP_Config" "REMOTE_HOST")
 REMOTE_DIR=$(get_config "SFTP_Config" "REMOTE_DIR")
 REMOTE_PORT=$(get_config "SFTP_Config" "REMOTE_PORT")
 
-rsync -avz --inplace -e "ssh -p $REMOTE_PORT" ${LOG_PATH}/*.log "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}"
+rsync -avz --rsync-path="/bin/rsync" --inplace -e "ssh -p $REMOTE_PORT" ${LOG_PATH}/*.log "${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIR}"
 
 echo "$(date) - Sync process completed."
 exit 0
