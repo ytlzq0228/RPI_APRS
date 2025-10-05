2e78e6dcf5a21370eb156563c6296334c318998f
liziqi <123>
2025-10-05 22:21:38 +0800

    支持静止状态和移动状态检测

--- APRS_Reporter.py
+++ APRS_Reporter.py
@@ -153,7 +153,7 @@ def traccar_report():
 
 				# 时间戳
 				ts = datetime.utcnow().replace(tzinfo=timezone.utc).isoformat().replace("+00:00", "Z")
-				payload={"id": str(SSID),"event":"heartbeat"}
+				payload={"id": str(SSID)}
 				still_wait_count+=1
 				if float(speed) > STILL_SPEED_THRESHOLD or still_wait_count>60:
 					still_wait_count=0
